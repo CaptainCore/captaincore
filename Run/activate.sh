6d3cf86b749b597c042c86297dd4ba3b1d571fed
Austin Ginder <austinginder@gmail.com>
2017-07-01 06:22:43 -0400

    New deactivate/activate commands

---
+++ Run/activate.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+##
+##      Removes custom deactivate mu-plugin to batch of installs
+##
+##      Pass arguments from command line like this
+##      Script/Run/activate.sh install1 install2
+##
+
+
+### Load configuration
+source ~/Scripts/config.sh
+
+# Paths
+plugin="anchorhost_deactivated"
+
+activate () {
+for (( i = 1; i <= $#; i++ ))
+do
+    var="$i"
+    website=${!var}
+
+    ### Load FTP credentials
+		source $path_scripts/logins.sh
+
+    ### Credentials found, start the backup
+    if ! [ -z "$domain" ]
+    then
+
+      ### remove deactivation plugin
+      lftp -e "set sftp:auto-confirm yes;set net:max-retries 2;set ftp:ssl-allow no;rm $homedir/wp-content/mu-plugins/$plugin.php; exit" -u $username,$password -p $port $protocol://$ipAddress
+
+    fi
+
+    ### Clear out variables
+    domain=''
+    username=''
+    password=''
+    ipAddress=''
+    protocol=''
+    port=''
+
+done
+
+}
+
+### See if any specific sites are selected
+if [ $# -gt 0 ]
+then
+    ## Run selected installs
+    activate $*
+fi
