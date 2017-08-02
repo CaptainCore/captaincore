e273ca9f4480046af9a33431eba96f41e88e3823
Austin Ginder <austinginder@gmail.com>
2017-08-01 20:25:39 -0400

    Show existing backup stats

--- Run/progress.sh
+++ Run/progress.sh
@@ -29,14 +29,23 @@ local_log=$( { ls ~/Logs/$most_recent_date/$most_recent_log/backup-local.txt; } 
 remote_log=$( { ls ~/Logs/$most_recent_date/$most_recent_log/backup-remote.txt; } 2>&1 )
 b2_log=$( { ls ~/Logs/$most_recent_date/$most_recent_log/backup-b2.txt; } 2>&1 )
 
+if [[ "$backup_log" != *"No such file or directory"* ]]; then
+
+  # Output bottom of backup log
+  printf "Selected Backup Log: \e[1;32m$backup_log\e[0m\n"
+  tail -4 $backup_log
+  printf "\n"
+
+fi
+
 # current FTP backup
 site_backup=`ls -rt ~/Logs/$most_recent_date/$most_recent_log/site-* | tail -1 | xargs -n 1 basename`
 site_backup_log=$( { ls ~/Logs/$most_recent_date/$most_recent_log/$site_backup; } 2>&1 )
 
 # Output sync status
 printf "Selected Site: \e[1;32m$site_backup_log\e[0m\n"
-tail -5 ~/Logs/$most_recent_date/$most_recent_log/$site_backup
-printf "\n"
+calc_site_backup_log=`php ~/Scripts/Get/log_stat.php log=$site_backup_log`
+echo $calc_site_backup_log | awk '{gsub("<br>","\n")};1'
 
 if [[ "$b2_log" != *"No such file or directory"* ]]; then
 
@@ -55,18 +64,9 @@ if [[ "$local_log" != *"No such file or directory"* ]]; then
   printf "Selected Local Log: \e[1;32m$local_log\e[0m\n"
 
   # Calculate log stats
-	calc_log_stats=`php ~/Scripts/Get/log_stat.php log=$local_log`
+	calc_log_stats=`php ~/Scripts/Get/transferred_stats.php file=$local_log`
   #calc_log_stats=`php ~/Scripts/Get/log_stats.php log=$local_log`
   echo $calc_log_stats | awk '{gsub("<br>","\n")};1'
-
-fi
-
-if [[ "$backup_log" != *"No such file or directory"* ]]; then
-
-  # Output bottom of backup log
-  printf "Selected Backup Log: \e[1;32m$backup_log\e[0m\n"
-  tail -4 $backup_log
-  printf "\n"
 
 fi
 
@@ -76,7 +76,7 @@ if [[ "$remote_log" != *"No such file or directory"* ]]; then
   printf "Selected Remote Log: \e[1;32m$remote_log\e[0m\n"
 
   # Calculate Remote transfer
-  calc_remote=`php ~/Scripts/Get/log_stat.php log=$remote_log`
+  calc_remote=`php ~/Scripts/Get/transferred_stats.php file=$remote_log`
   echo $calc_remote | awk '{gsub("<br>","\n")};1'
 
 fi
