cc5bf41937e4cf16cfbd26c5baa65712e1dc0c83
Austin Ginder <austinginder@gmail.com>
2017-08-04 08:50:01 -0400

    Only display results if matches found.

--- Get/log_stat.php
+++ Get/log_stat.php
@@ -53,88 +53,93 @@ function secs_to_str($duration) {
         return join(', ', $parts) . " and " . $last;
 }
 
-// Last item in array
-$match_count = count($matches[0]) - 1;
+// If matches found then output results
+if (count($matches[0]) > 0) {
 
-##    Finds last match. Example:
-##
-##    Transferred:   2.189 GBytes (166.310 kBytes/s)
-##    Errors:                 0
-##    Checks:             13958
-##    Transferred:        13954
-##    Elapsed time:   3h50m0.6s
+  // Last item in array
+  $match_count = count($matches[0]) - 1;
 
-$last_match = $matches[0][$match_count];
+  ##    Finds last match. Example:
+  ##
+  ##    Transferred:   2.189 GBytes (166.310 kBytes/s)
+  ##    Errors:                 0
+  ##    Checks:             13958
+  ##    Transferred:        13954
+  ##    Elapsed time:   3h50m0.6s
 
-// Bytes
-$pattern = '/(\d.*)(?= Bytes )/';
-preg_match_all($pattern, $last_match, $matches);
-$total_bytes = array_sum($matches[0]);
+  $last_match = $matches[0][$match_count];
 
-// KBs
-$pattern = '/(\d.*)(?= kBytes )/';
-preg_match_all($pattern, $last_match, $matches);
-$total_kbytes = array_sum($matches[0]);
+  // Bytes
+  $pattern = '/(\d.*)(?= Bytes )/';
+  preg_match_all($pattern, $last_match, $matches);
+  $total_bytes = array_sum($matches[0]);
 
-// MBs
-$pattern = '/(\d.*)(?= MBytes )/';
-preg_match_all($pattern, $last_match, $matches);
-$total_mbytes = array_sum($matches[0]);
+  // KBs
+  $pattern = '/(\d.*)(?= kBytes )/';
+  preg_match_all($pattern, $last_match, $matches);
+  $total_kbytes = array_sum($matches[0]);
 
-// GBs
-$pattern = '/(\d.*)(?= GBytes )/';
-preg_match_all($pattern, $last_match, $matches);
-$total_gbytes = array_sum($matches[0]);
+  // MBs
+  $pattern = '/(\d.*)(?= MBytes )/';
+  preg_match_all($pattern, $last_match, $matches);
+  $total_mbytes = array_sum($matches[0]);
 
-// Add it all up
-$total_gb = round($total_bytes / 1024 / 1024 / 1024, 2) + round($total_kbytes / 1024 / 1024, 2) + round($total_mbytes / 1024, 2) + round($total_gbytes, 2);
+  // GBs
+  $pattern = '/(\d.*)(?= GBytes )/';
+  preg_match_all($pattern, $last_match, $matches);
+  $total_gbytes = array_sum($matches[0]);
 
-// Errors
-$pattern = '/(\d.*)(?=\sChecks)/';
-preg_match_all($pattern, $last_match, $matches);
-$total_errors = array_sum($matches[0]);
+  // Add it all up
+  $total_gb = round($total_bytes / 1024 / 1024 / 1024, 2) + round($total_kbytes / 1024 / 1024, 2) + round($total_mbytes / 1024, 2) + round($total_gbytes, 2);
 
-// Checks
-$pattern = '/(\d.*)(?=\sTransferred)/';
-preg_match_all($pattern, $last_match, $matches);
-$total_checks = array_sum($matches[0]);
+  // Errors
+  $pattern = '/(\d.*)(?=\sChecks)/';
+  preg_match_all($pattern, $last_match, $matches);
+  $total_errors = array_sum($matches[0]);
 
-// Transferred
-$pattern = '/(\d.*)(?=\sElapsed time)/';
-preg_match_all($pattern, $last_match, $matches);
-$total_transferred = array_sum($matches[0]);
+  // Checks
+  $pattern = '/(\d.*)(?=\sTransferred)/';
+  preg_match_all($pattern, $last_match, $matches);
+  $total_checks = array_sum($matches[0]);
 
-// Elapsed time
-$pattern = '/(?:Elapsed time:\s+)(\d.*)/';
-preg_match_all($pattern, $last_match, $matches);
-$elapsed_time = $matches[1][0];
+  // Transferred
+  $pattern = '/(\d.*)(?=\sElapsed time)/';
+  preg_match_all($pattern, $last_match, $matches);
+  $total_transferred = array_sum($matches[0]);
 
-$total_time_in_seconds = 0;
+  // Elapsed time
+  $pattern = '/(?:Elapsed time:\s+)(\d.*)/';
+  preg_match_all($pattern, $last_match, $matches);
+  $elapsed_time = $matches[1][0];
 
-  // Search for hours
-  if (strpos($elapsed_time, 'h') !== false) {
-    $pattern = '/(.+)(?:h)(.+)(?:m)(.+)(?:s)/';
-    preg_match_all($pattern, $elapsed_time, $matches);
-    $hours = $matches[1][0] * 60 * 60;
-    $minutes = $matches[2][0] * 60;
-    $seconds = $matches[3][0] + $hours + $minutes;
-  // Search for minutes
-  } elseif (strpos($elapsed_time, 'm') !== false) {
-    $pattern = '/(.+)(?:m)(.+)(?:s)/';
-    preg_match_all($pattern, $elapsed_time, $matches);
-    $minutes = $matches[1][0] * 60;
-    $seconds = $matches[2][0] + $minutes;
-  // Search for seconds
-  } elseif (strpos($elapsed_time, 's') !== false) {
-    $pattern = '/(.+)(?:s)/';
-    preg_match_all($pattern, $elapsed_time, $matches);
-    $seconds = $matches[1][0];
+  $total_time_in_seconds = 0;
+
+    // Search for hours
+    if (strpos($elapsed_time, 'h') !== false) {
+      $pattern = '/(.+)(?:h)(.+)(?:m)(.+)(?:s)/';
+      preg_match_all($pattern, $elapsed_time, $matches);
+      $hours = $matches[1][0] * 60 * 60;
+      $minutes = $matches[2][0] * 60;
+      $seconds = $matches[3][0] + $hours + $minutes;
+    // Search for minutes
+    } elseif (strpos($elapsed_time, 'm') !== false) {
+      $pattern = '/(.+)(?:m)(.+)(?:s)/';
+      preg_match_all($pattern, $elapsed_time, $matches);
+      $minutes = $matches[1][0] * 60;
+      $seconds = $matches[2][0] + $minutes;
+    // Search for seconds
+    } elseif (strpos($elapsed_time, 's') !== false) {
+      $pattern = '/(.+)(?:s)/';
+      preg_match_all($pattern, $elapsed_time, $matches);
+      $seconds = $matches[1][0];
+    }
+    $total_time_in_seconds = $total_time_in_seconds + $seconds;
+
+  $total_time = secs_to_str($total_time_in_seconds);
+
+  // return GBs transferred
+  if ($total_time) {
+    echo $total_gb ." GB - " . $total_errors . " errors - " . $total_checks . " checks - ". $total_transferred . " transferred - ". $total_time;
   }
-  $total_time_in_seconds = $total_time_in_seconds + $seconds;
 
-$total_time = secs_to_str($total_time_in_seconds);
-
-// return GBs transferred
-if ($total_time) {
-  echo $total_gb ." GB - " . $total_errors . " errors - " . $total_checks . " checks - ". $total_transferred . " transferred - ". $total_time;
 }
