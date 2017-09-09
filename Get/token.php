809eb5a7378e33678ec2f080e4d3f97e6279f6cc
Austin Ginder <austinginder@gmail.com>
2017-09-09 10:03:12 -0400

    Argument to get token per file

--- Get/token.php
+++ Get/token.php
@@ -1,19 +1,33 @@
 <?php
 
 //  Pass arguments from command line like this
-//  php get.php domain=anchor.host
+//  php Scripts/Get/token.php domain=anchor.host
+//  php Scripts/Get/token.php file=~/Sites/anchor.dev/wp-config.php
 
 parse_str(implode('&', array_slice($argv, 1)), $_GET);
 
-$file = $_SERVER['HOME'] . "/Backup/" . $_GET['domain']. "/wp-config.php";
+if (isset($_GET['domain'])) {
+	$file = $_SERVER['HOME'] . "/Backup/" . $_GET['domain']. "/wp-config.php";
 
-if (file_exists($file)) {
-	$wp_config = file_get_contents($file);
+	if (file_exists($file)) {
+		$wp_config = file_get_contents($file);
 
-	preg_match('/define.*AUTH_KEY.*\'(.*)\'/', $wp_config, $value);
-	$auth_key = $value[1];
+		preg_match('/define.*AUTH_KEY.*\'(.*)\'/', $wp_config, $value);
+		$auth_key = $value[1];
 
-	echo md5($auth_key);
+		echo md5($auth_key);
+	}
 }
 
-?>
+if (isset($_GET['file'])) {
+	$file = $_GET['file'];
+
+	if (file_exists($file)) {
+		$wp_config = file_get_contents($file);
+
+		preg_match('/define.*AUTH_KEY.*\'(.*)\'/', $wp_config, $value);
+		$auth_key = $value[1];
+
+		echo md5($auth_key);
+	}
+}
