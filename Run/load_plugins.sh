4b2c9561344cc814334fba3db242bab44efbb8ce
Austin Ginder <austinginder@gmail.com>
2017-11-17 16:33:10 -0500

    Added dynamic plugins to static plugin uploader

--- Run/load_plugins.sh
+++ Run/load_plugins.sh
@@ -1,4 +1,13 @@
-### Load configuration 
+#!/bin/bash
+
+##
+##      Preloading plugins to install
+##
+##      Pass arguments from command line like this
+##      Script/Run/load_plugin.sh install1 install2
+##
+
+### Load configuration
 source ~/Scripts/config.sh
 
 # Paths
@@ -14,19 +23,33 @@ then
 		var="$i"
 		website=${!var}
 
-		### Load FTP credentials 
+		### Load FTP credentials
 		source ~/Scripts/logins.sh
 
 		### Credentials found, start the backup
 		if ! [ -z "$domain" ]
 		then
+
+			### Loop through static list of plugins
 			for plugin in akismet worker jetpack anchorhost-client gravityforms google-analytics-for-***REMOVED***
 			do
-				### Incremental backup download to local file system
+				### Upload plugin
 				lftp -e "set sftp:auto-confirm yes;set net:max-retries 2;set net:reconnect-interval-base 5;set net:reconnect-interval-multiplier 1;mirror --only-newer --delete --reverse --parallel=2 --exclude .git/ --exclude .DS_Store --exclude Thumbs.db --verbose=2 $local_path/$plugin $remote_path/$plugin; exit" -u $username,$password -p $port $protocol://$ipAddress
 			done
 
-			# mail -s "Plugins loaded: $domain | $(date +'%Y-%m-%d')" support@anchor.host
+			### Loads token
+			website_token=$(php ~/Scripts/Get/token.php domain=$domain)
+
+			### Load dynamic list of plugins for install
+			plugins=`php ~/Scripts/Run/plugin-generate.php token=$website_token customers=$preloadusers`
+
+			### Loop through dynamic list of plugins
+			for plugin in $plugins
+			do
+				### Upload plugin
+				lftp -e "set sftp:auto-confirm yes;set net:max-retries 2;set net:reconnect-interval-base 5;set net:reconnect-interval-multiplier 1;mirror --only-newer --delete --reverse --parallel=2 --exclude .git/ --exclude .DS_Store --exclude Thumbs.db --verbose=2 $local_path/$plugin $remote_path/$plugin; exit" -u $username,$password -p $port $protocol://$ipAddress
+			done
+
 		fi
 
 		### Clear out variables
