833ff518974d9a0058fb20f5284e5e125067c82b
Austin Ginder <austinginder@gmail.com>
2018-05-06 20:52:55 -0400

    Adds site_id to get output

--- lib/php/site-get.php
+++ lib/php/site-get.php
@@ -5,6 +5,7 @@ $website    = get_post( $website_id );
 $fields     = array( 'install', 'address', 'username', 'protocol', 'port', 'homedir', 'database_username', 'database_password', 'install_staging', 'address_staging', 'username_staging', 'protocol_staging', 'port_staging', 'homedir_staging', 'database_username_staging', 'database_password_staging', 's3accesskey', 's3secretkey', 's3bucket', 's3path', 'preloadusers', 'home_url' );
 $title      = $website->post_title;
 
+echo "site_id=$website_id\n";
 echo "domain=$title\n";
 foreach ( $fields as $field ) {
 	$value = get_post_meta( $website_id, $field, true );
