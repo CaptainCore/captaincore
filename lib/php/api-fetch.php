da97ccaffcf78e451fa1bce1d1df75a12e5d941c
Austin Ginder <austinginder@gmail.com>
2018-07-12 11:09:29 -0400

    Renamed config  captaincore_wordpress_site to captaincore_gui.

--- lib/php/api-fetch.php
+++ lib/php/api-fetch.php
@@ -3,7 +3,7 @@
 // Converts arguments --staging --all --plugin=woocommerce --theme=anchorhost into $staging $all
 parse_str( implode( '&', $args ) );
 
-$curl = curl_init( $captaincore_wordpress_site . "/wp-json/captaincore/v1/client" );
+$curl = curl_init( $captaincore_gui . "/wp-json/captaincore/v1/client" );
 curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
 $response = curl_exec( $curl );
 $response = json_decode( $response, true );
