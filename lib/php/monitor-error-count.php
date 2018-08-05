317e5acd24c2efe989af4fb844322bc27b9ce6cf
Austin Ginder <austinginder@gmail.com>
2018-08-05 11:48:25 -0400

    👌 IMPROVE: Handle html errors.

--- lib/php/monitor-error-count.php
+++ lib/php/monitor-error-count.php
@@ -9,25 +9,34 @@ $lines = explode("\n", $contents);
 foreach ($lines as $line) {
   $json = json_decode( $line );
 
-	if (json_last_error() !== JSON_ERROR_NONE ) {
-    # JSON is valid
-		continue;
-	}
+	# Check if JSON valid
+  if (json_last_error() !== JSON_ERROR_NONE ) {
+    continue;
+  }
 
-	# Just became healthy. Update status and mark new health
-	if ( $json->http_code == "200" ) {
+  $http_code = $json->http_code;
+  $url = $json->url;
+  $html_valid = $json->html_valid;
 
-		continue;
-	}
+  # Check if HTML is valid
+  if ( $html_valid == "false" ) {
+    $errors[] = "Response code $http_code for $url html is invalid\n";
+    continue;
+  }
 
-	# Handle redirects
-	if ( $json->http_code == "301" ) {
+  # Check if healthy
+  if ( $json->http_code == "200" ) {
 
-		continue;
-	}
+    continue;
+  }
 
-	# Append error to errors for email purposes
-	$errors[] = "Response code $http_code for $url\n";
+  # Check for redirects
+  if ( $json->http_code == "301" ) {
+    continue;
+  }
+
+  # Append error to errors for email purposes
+  $errors[] = "Response code $http_code for $url\n";
 }
 
 echo count($errors);
--- lib/php/monitor-generate-email.php
+++ lib/php/monitor-generate-email.php
@@ -10,44 +10,52 @@ $lines = explode("\n", $contents);
 foreach ($lines as $line) {
   $json = json_decode( $line );
 
-	if (json_last_error() !== JSON_ERROR_NONE ) {
-    # JSON is valid
-		continue;
-	}
+  # Check if JSON valid
+  if (json_last_error() !== JSON_ERROR_NONE ) {
+    continue;
+  }
 
-	$http_code = $json->http_code;
-	$url = $json->url;
+  $http_code = $json->http_code;
+  $url = $json->url;
+  $html_valid = $json->html_valid;
 
-	# Just became healthy. Update status and mark new health
-	if ( $json->http_code == "200" ) {
+  # Check if HTML is valid
+  if ( $html_valid == "false" ) {
+    $errors[] = "Response code $http_code for $url html is invalid\n";
+    continue;
+  }
 
-		continue;
-	}
+  # Check if healthy
+  if ( $json->http_code == "200" ) {
 
-	# Handle redirects
-	if ( $json->http_code == "301" ) {
-		$warnings[] = "Response code $http_code for $url\n";
-		continue;
-	}
+    continue;
+  }
 
-	# Append error to errors for email purposes
-	$errors[] = "Response code $http_code for $url\n";
+  # Check for redirects
+  if ( $json->http_code == "301" ) {
+    $warnings[] = "Response code $http_code for $url\n";
+    continue;
+  }
+
+  # Append error to errors for email purposes
+  $errors[] = "Response code $http_code for $url\n";
 }
 
+# if errors then generate html
 if ( count($errors) > 0 ) {
 
-	$html = "<strong>Errors</strong><br /><br />";
+  $html = "<strong>Errors</strong><br /><br />";
 
-	foreach ($errors as $error) {
-		$html .= trim($error) . "<br />\n";
-	};
+  foreach ($errors as $error) {
+    $html .= trim($error) . "<br />\n";
+  };
 
-	$html .= "<br /><strong>Warnings</strong><br /><br />";
+  $html .= "<br /><strong>Warnings</strong><br /><br />";
 
-	foreach ($warnings as $warning) {
-		$html .= trim($warning) . "<br />\n";
-	};
+  foreach ($warnings as $warning) {
+    $html .= trim($warning) . "<br />\n";
+  };
 
-	echo $html;
+  echo $html;
 
 }
