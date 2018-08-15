fc88ad859b4e90d79e8af96bf8e93b7f47edd079
Austin Ginder <austinginder@gmail.com>
2018-08-14 20:52:04 -0400

    👌 IMPROVE: Whitespace

--- lib/local-scripts/monitor-error-count.php
+++ lib/local-scripts/monitor-error-count.php
@@ -1,42 +1,39 @@
 <?php
 
-$errors = array();
-
+$errors   = array();
 $contents = file_get_contents( $args[0] );
+$lines    = explode( "\n", $contents );
 
-$lines = explode("\n", $contents);
+foreach ( $lines as $line ) {
+	$json = json_decode( $line );
 
-foreach ($lines as $line) {
-  $json = json_decode( $line );
+	// Check if JSON valid
+	if ( json_last_error() !== JSON_ERROR_NONE ) {
+		continue;
+	}
 
-	# Check if JSON valid
-  if (json_last_error() !== JSON_ERROR_NONE ) {
-    continue;
-  }
+	$http_code  = $json->http_code;
+	$url        = $json->url;
+	$html_valid = $json->html_valid;
 
-  $http_code = $json->http_code;
-  $url = $json->url;
-  $html_valid = $json->html_valid;
+	// Check if HTML is valid
+	if ( $html_valid == 'false' ) {
+		$errors[] = "Response code $http_code for $url html is invalid\n";
+		continue;
+	}
 
-  # Check if HTML is valid
-  if ( $html_valid == "false" ) {
-    $errors[] = "Response code $http_code for $url html is invalid\n";
-    continue;
-  }
+	// Check if healthy
+	if ( $json->http_code == '200' ) {
+		continue;
+	}
 
-  # Check if healthy
-  if ( $json->http_code == "200" ) {
+	// Check for redirects
+	if ( $json->http_code == '301' ) {
+		continue;
+	}
 
-    continue;
-  }
-
-  # Check for redirects
-  if ( $json->http_code == "301" ) {
-    continue;
-  }
-
-  # Append error to errors for email purposes
-  $errors[] = "Response code $http_code for $url\n";
+	// Append error to errors for email purposes
+	$errors[] = "Response code $http_code for $url\n";
 }
 
-echo count($errors);
+echo count( $errors );
--- lib/local-scripts/monitor-generate-email.php
+++ lib/local-scripts/monitor-generate-email.php
@@ -1,61 +1,58 @@
 <?php
 
-$errors = array();
+$errors   = array();
 $warnings = array();
-
 $contents = file_get_contents( $args[0] );
+$lines    = explode( "\n", $contents );
 
-$lines = explode("\n", $contents);
+foreach ( $lines as $line ) {
+	$json = json_decode( $line );
 
-foreach ($lines as $line) {
-  $json = json_decode( $line );
+	// Check if JSON valid
+	if ( json_last_error() !== JSON_ERROR_NONE ) {
+		continue;
+	}
 
-  # Check if JSON valid
-  if (json_last_error() !== JSON_ERROR_NONE ) {
-    continue;
-  }
+	$http_code  = $json->http_code;
+	$url        = $json->url;
+	$html_valid = $json->html_valid;
 
-  $http_code = $json->http_code;
-  $url = $json->url;
-  $html_valid = $json->html_valid;
+	// Check if HTML is valid
+	if ( $html_valid == 'false' ) {
+		$errors[] = "Response code $http_code for $url html is invalid\n";
+		continue;
+	}
 
-  # Check if HTML is valid
-  if ( $html_valid == "false" ) {
-    $errors[] = "Response code $http_code for $url html is invalid\n";
-    continue;
-  }
+	// Check if healthy
+	if ( $json->http_code == '200' ) {
+		continue;
+	}
 
-  # Check if healthy
-  if ( $json->http_code == "200" ) {
+	// Check for redirects
+	if ( $json->http_code == '301' ) {
+		$warnings[] = "Response code $http_code for $url\n";
+		continue;
+	}
 
-    continue;
-  }
-
-  # Check for redirects
-  if ( $json->http_code == "301" ) {
-    $warnings[] = "Response code $http_code for $url\n";
-    continue;
-  }
-
-  # Append error to errors for email purposes
-  $errors[] = "Response code $http_code for $url\n";
+	// Append error to errors for email purposes
+	$errors[] = "Response code $http_code for $url\n";
 }
 
-# if errors then generate html
-if ( count($errors) > 0 ) {
+// if errors then generate html
+if ( count( $errors ) > 0 ) {
 
-  $html = "<strong>Errors</strong><br /><br />";
+	$html = '<strong>Errors</strong><br /><br />';
 
-  foreach ($errors as $error) {
-    $html .= trim($error) . "<br />\n";
-  };
+	foreach ( $errors as $error ) {
+		$html .= trim( $error ) . "<br />\n";
+	};
 
-  $html .= "<br /><strong>Warnings</strong><br /><br />";
+	$html .= '<br /><strong>Warnings</strong><br /><br />';
 
-  foreach ($warnings as $warning) {
-    $html .= trim($warning) . "<br />\n";
-  };
+	foreach ( $warnings as $warning ) {
+		$html .= trim( $warning ) . "<br />\n";
+	};
 
-  echo $html;
+	echo $html;
 
 }
