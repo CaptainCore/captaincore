1e1caef9740f6010e40aa2eda764ae984cb365f4
Austin Ginder <austinginder@gmail.com>
2019-03-02 16:25:53 -0500

    📦 NEW: Environment support

--- lib/local-scripts/site-get.php
+++ lib/local-scripts/site-get.php
@@ -3,6 +3,11 @@
 // Converts arguments into variables
 parse_str( implode( '&', $args ) );
 
+// Assign default format to JSON
+if ( $format == "" ) {
+	$format = "json";
+}
+
 // WP_Query arguments
 $arguments = array(
 	'post_type'      => array( 'captcore_website' ),
@@ -32,57 +37,119 @@ $site_ids = get_posts( $arguments );
 
 // Bash output
 foreach ( $site_ids as $site_id ) {
-	$site   = get_post( $site_id );
-	$fields = array( 'ID', 'domain', 'site', 'provider', 'fathom', 'address', 'username', 'protocol', 'port', 'homedir', 'database_username', 'database_password', 'site_staging', 'address_staging', 'username_staging', 'protocol_staging', 'port_staging', 'homedir_staging', 'database_username_staging', 'database_password_staging', 's3accesskey', 's3secretkey', 's3bucket', 's3path', 'preloadusers', 'home_url', 'updates_enabled', 'exclude_themes', 'exclude_plugins' );
-	if ( $field ) {
-		$fields = array( $field );
+
+	// Bail if enviroment not set
+	if ( ! $environment == "production" or ! $environment == "staging" ) {
+		return;
 	}
-	$title = $site->post_title;
 
-	$json = '[{';
+	$title        = get_the_title( $site_id );
+	$site         = get_post_meta( $site_id, "site", true );
+	$provider     = get_post_meta( $site_id, "provider", true );
+	$fathom       = get_post_meta( $site_id, "fathom", true );
+	$preloadusers = get_post_meta( $site_id, "preloadusers", true );
 
-	$bash = '';
-
-	foreach ( $fields as $f ) {
-		if ( $f == 'ID' ) {
-			$value = get_post_meta( $site_id, $f, true );
-			$bash .= "site_id=$site_id\n";
-			$json .= "\"ID\":\"$site_id\",";
-		} elseif ( $f == 'domain' ) {
-			$bash .= "domain=$title\n";
-			$json .= "\"domain\":\"$title\",";
-		} elseif ( $f == 'fathom' ) {
-			$value = get_post_meta( $site_id, $f, true );
-			$bash .= "fathom=$value\n";
-			if ( $value ) {
-				$json .= "\"fathom\":${value},";
-			} else {
-				$json .= "\"fathom\":\"\",";
-			}
-		} else {
-			$value = get_post_meta( $site_id, $f, true );
-			$bash .= "$f=$value\n";
-			$json .= "\"$f\":\"$value\",";
-		}
+	if ( $environment == "production" ) {
+		$address                 = get_post_meta( $site_id, "address", true );
+		$username                = get_post_meta( $site_id, "username", true );
+		$protocol                = get_post_meta( $site_id, "protocol", true );
+		$port                    = get_post_meta( $site_id, "port", true );
+		$home_directory          = get_post_meta( $site_id, "home_directory", true );
+		$database_username       = get_post_meta( $site_id, "database_username", true );
+		$database_password       = get_post_meta( $site_id, "database_password", true );
+		$offload_enabled         = get_post_meta( $site_id, "offload_enabled", true );
+		$offload_provider        = get_post_meta( $site_id, "offload_provider", true );
+		$offload_access_key      = get_post_meta( $site_id, "offload_access_key", true );
+		$offload_secret_key      = get_post_meta( $site_id, "offload_secret_key", true );
+		$offload_bucket          = get_post_meta( $site_id, "offload_bucket", true );
+		$offload_path            = get_post_meta( $site_id, "offload_path", true );
+		$home_url                = get_post_meta( $site_id, "home_url", true );
+		$updates_enabled         = get_post_meta( $site_id, "updates_enabled", true );
+		$updates_exclude_themes  = get_post_meta( $site_id, "updates_exclude_themes", true );
+		$updates_exclude_plugins = get_post_meta( $site_id, "updates_exclude_plugins", true );
 	}
-	$json  = substr( $json, 0, -1 );
-	$json .= '}]';
+
+	if ( $environment == "staging" ) {
+		$address                 = get_post_meta( $site_id, "address_staging", true );
+		$username                = get_post_meta( $site_id, "username_staging", true );
+		$protocol                = get_post_meta( $site_id, "protocol_staging", true );
+		$port                    = get_post_meta( $site_id, "port_staging", true );
+		$home_directory          = get_post_meta( $site_id, "home_directory_staging", true );
+		$database_username       = get_post_meta( $site_id, "database_username_staging", true );
+		$database_password       = get_post_meta( $site_id, "database_password_staging", true );
+		$offload_enabled         = get_post_meta( $site_id, "offload_enabled_staging", true );
+		$offload_provider        = get_post_meta( $site_id, "offload_provider_staging", true );
+		$offload_access_key      = get_post_meta( $site_id, "offload_access_key_staging", true );
+		$offload_secret_key      = get_post_meta( $site_id, "offload_secret_key_staging", true );
+		$offload_bucket          = get_post_meta( $site_id, "offload_bucket_staging", true );
+		$offload_path            = get_post_meta( $site_id, "offload_path_staging", true );
+		$home_url                = get_post_meta( $site_id, "home_url_staging", true );
+		$updates_enabled         = get_post_meta( $site_id, "updates_enabled_staging", true );
+		$updates_exclude_themes  = get_post_meta( $site_id, "updates_exclude_themes_staging", true );
+		$updates_exclude_plugins = get_post_meta( $site_id, "updates_exclude_plugins_staging", true );
+	}
+
+	$array = array(
+		"ID"                      => $site_id,
+		"site"                    => $site,
+		"provider"                => $provider,
+		"preloadusers"            => $preloadusers,
+		"home_url"                => $home_url,
+		"domain"                  => $title,
+		"fathom"                  => $fathom,
+		'address'                 => $address,
+		'username'                => $username,
+		'password'                => $password,
+		'protocol'                => $protocol,
+		'port'                    => $port,
+		'home_directory'          => $home_directory,
+		'database_username'       => $database_username,
+		'database_password'       => $database_password,
+		'updates_enabled'         => $updates_enabled,
+		'updates_exclude_themes'  => $updates_exclude_themes,
+		'updates_exclude_plugins' => $updates_exclude_plugins,
+		'offload_enabled'         => $offload_enabled,
+		'offload_provider'        => $offload_provider,
+		'offload_access_key'      => $offload_access_key,
+		'offload_secret_key'      => $offload_secret_key,
+		'offload_bucket'          => $offload_bucket,
+		'offload_path'            => $offload_path,
+	);
+
+	$bash = "site_id=$site_id
+domain=$title
+fathom=$fathom
+site=$site
+provider=$provider
+preloadusers=$preloadusers
+home_url=$home_url
+address=$address
+username=$username
+protocol=$protocol
+port=$port
+home_directory=$home_directory
+database_username=$database_username
+database_password=$database_password
+updates_enabled=$updates_enabled
+updates_exclude_themes=$updates_exclude_themes
+updates_exclude_plugins=$updates_exclude_plugins
+offload_enabled=$offload_enabled
+offload_provider=$offload_provider
+offload_access_key=$offload_access_key
+offload_secret_key=$offload_secret_key
+offload_bucket=$offload_bucket
+offload_path=$offload_path";
 
 }
 if ( $field ) {
-	if ( $field == 'ID' ) {
-		$value = get_post_meta( $site_id, $f, true );
-		$json = $site_id;
-	} elseif ( $field == 'domain' ) {
-		$json = $title;
-	} else {
-		$value = get_post_meta( $site_id, $field, true );
-		$json = $value;
-	}
+	echo $array[$field];
+	return true;
 }
 
 if ( $format == 'bash' ) {
-		echo $bash;
-} else {
-		echo $json;
+	echo $bash;
+} 
+
+if ( $format == 'json' ) {
+	echo json_encode($array, JSON_PRETTY_PRINT);
 }
