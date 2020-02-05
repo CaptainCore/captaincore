74a545e05045f956e1253ede8dede6ee4db2f32f
Austin Ginder <austinginder@gmail.com>
2020-02-05 13:20:03 -0500

    👌 IMPROVE: Site add and update

--- lib/local-scripts/site-add.php
+++ lib/local-scripts/site-add.php
@@ -25,7 +25,9 @@ if ( $format == 'base64' ) {
 	$site         = json_decode( base64_decode( $details ) ) ;
 	$site_check   = ( new CaptainCore\Sites )->get( $site->site_id );
 	$environments = $site->environments;
+	$shared_with  = $site->shared_with;
 	unset( $site->environments );
+	unset( $site->shared_with );
 	if ( empty( $site_check ) ) {
 		// Insert new site
 		( new CaptainCore\Sites )->insert( (array) $site );
@@ -43,6 +45,17 @@ if ( $format == 'base64' ) {
 		}
 		// Update existing environment
 		( new CaptainCore\Environments )->update( (array) $environment, [ "environment_id" => $environment_id ] );
+	}
+	foreach ( $shared_with as $record ) {
+		$account_site_id    = $record->account_site_id;
+		$account_site_check = ( new CaptainCore\AccountSite )->get( $account_site_id );
+		// Insert new environment
+		if ( empty( $account_site_check ) ) {
+			( new CaptainCore\AccountSite )->insert( (array) $record );
+			continue;
+		}
+		// Update existing environment
+		( new CaptainCore\AccountSite )->update( (array) $record, [ "account_site_id" => $account_site_id ] );
 	}
 
 }
