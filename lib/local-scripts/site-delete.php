47da0937b20c58c1e91847b15cf812229097d65f
Austin Ginder <austinginder@gmail.com>
2020-02-05 16:17:52 -0500

    👌 IMPROVE: Site delete

--- lib/local-scripts/site-delete.php
+++ lib/local-scripts/site-delete.php
@@ -40,43 +40,13 @@ foreach($config_data as $config) {
 // Fetch from CLI configs
 $captaincore_admin_email = $configuration->vars->captaincore_admin_email;
 
-// Build arguments
-$arguments = array(
-	'author'    	 => $captain_id,
-	'post_type'      => 'captcore_website',
-	'posts_per_page' => '1',
-	'meta_query'     => array(
-		array(
-			'key'     => 'site_id',
-			'value'   => $id,
-			'compare' => '=',
-		),
-	),
-);
-// Check if site
-$found_site = get_posts( $arguments );
+$site = ( new CaptainCore\Sites )->get( $site_id );
 
-if ( $found_site ) {
+if ( $site ) {
+	// Make final snapshot then remove local files
+	$output = shell_exec( "captaincore snapshot $site --delete-after-snapshot --email=$captaincore_admin_email --captain_id=$captain_id" );
 
-	$found_site_id = $found_site[0]->ID;
-
-	$my_post = array(
-
-		'ID'          => $found_site_id,
-		'post_status' => 'publish',
-		'meta_input'  => array(
-			'status' => 'closed',
-		),
-	);
-	echo "Site removed\n";
-
-	wp_update_post( $my_post );
-
-} else {
-
-	echo "Site not found\n";
-
+	// Mark for delection
+	( new CaptainCore\Site( $site_id, true ) )->delete();
 }
 
-// Make final snapshot then remove local files
-$output = shell_exec( "captaincore snapshot $site --delete-after-snapshot --email=$captaincore_admin_email --captain_id=$captain_id > /dev/null 2>/dev/null &" );
