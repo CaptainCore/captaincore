75cf70e1bca4829b80d926d7cd250351ead123b5
Austin Ginder <austinginder@gmail.com>
2020-03-15 21:14:23 -0400

    📦 NEW: Command `default-sync`

---
+++ lib/local-scripts/default-sync.php
@@ -0,0 +1,73 @@
+<?php
+
+// Replaces dashes in keys with underscores
+foreach($args as $index => $arg) {
+	$split = strpos($arg, "=");
+	if ( $split ) {
+		$key = str_replace('-', '_', substr( $arg , 0, $split ) );
+		$value = substr( $arg , $split, strlen( $arg ) );
+
+		// Removes unnecessary bash quotes
+		$value = trim( $value,'"' ); 				// Remove last quote 
+		$value = str_replace( '="', '=', $value );  // Remove quote right after equals
+
+		$args[$index] = $key.$value;
+	} else {
+		$args[$index] = str_replace('-', '_', $arg);
+	}
+
+}
+
+// Converts --arguments into $arguments
+parse_str( implode( '&', $args ) );
+
+// Loads CLI configs
+$json = "{$_SERVER['HOME']}/.captaincore-cli/config.json";
+
+if ( ! file_exists( $json ) ) {
+	echo "Error: Configuration file not found.";
+	return;
+}
+
+$config_data = json_decode ( file_get_contents( $json ) );
+$system      = $config_data[0]->system;
+$path        = $system->path;
+
+foreach($config_data as $config) {
+	if ( isset( $config->captain_id ) and $config->captain_id == $captain_id ) {
+		$configuration = $config;
+		break;
+	}
+}
+
+// Prepare request to API
+$request = [
+    'method'  => 'POST',
+    'headers' => [ 'Content-Type' => 'application/json' ],
+    'body'    => json_encode( [ 
+        "command" => "default-get",
+        "token"   => $configuration->keys->token,
+    ] ),
+];
+
+if ( ! empty( $system->captaincore_dev ) ) {
+    $request['sslverify'] = false;
+}
+
+// Post to CaptainCore API
+$response = wp_remote_post( $configuration->vars->captaincore_api, $request );
+if ( is_wp_error( $response ) ) {
+    $error_message = $response->get_error_message();
+    return "Something went wrong: $error_message";
+}
+
+$results = json_decode( $response['body'] );
+
+if ( $debug !== null ) {
+    echo json_encode( $results, JSON_PRETTY_PRINT );
+    return;
+}
+
+echo json_encode( $results, JSON_PRETTY_PRINT );
+
+update_site_option( 'captaincore_defaults', json_encode( $results->defaults ) );
