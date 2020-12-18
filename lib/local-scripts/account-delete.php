453ab908bf8313007920c4b5b8f809bb1e755f4d
Austin Ginder <austinginder@gmail.com>
2020-12-18 18:22:25 -0500

    📦 NEW: Command `account delete`

---
+++ lib/local-scripts/account-delete.php
@@ -0,0 +1,29 @@
+<?php
+
+// Replaces dashes in keys with underscores
+foreach($args as $index => $arg) {
+	$split = strpos($arg, "=");
+	if ( $split ) {
+		$key = str_replace('-', '_', substr( $arg , 0, $split ) );
+		$value = substr( $arg , $split, strlen( $arg ) );
+
+		// Removes unnecessary bash quotes
+		$value = trim( $value,'"' ); 				// Remove last quote 
+		$value = str_replace( '="', '=', $value );  // Remove quote right after equals
+
+		$args[$index] = $key.$value;
+	} else {
+		$args[$index] = str_replace('-', '_', $arg);
+	}
+
+}
+
+// Converts --arguments into $arguments
+parse_str( implode( '&', $args ) );
+
+$account = ( new CaptainCore\Accounts )->get( $account_id );
+
+if ( $account ) {
+	( new CaptainCore\Account( $account_id, true ) )->delete();
+}
+
