0b41ce0ad7c88c59d9d21d1072bce8558e72abe7
Austin Ginder <austinginder@gmail.com>
2024-05-05 06:40:26 -0400

    👌 IMPROVE: Remove legacy update logs

--- lib/local-scripts/site-run-updates.php
+++ lib/local-scripts/site-run-updates.php
@@ -21,7 +21,13 @@ foreach($args as $index => $arg) {
 }
 
 // Converts --arguments into $arguments
-parse_str( implode( '&', $args ) );
+parse_str( implode( '&', $args ), $arguments );
+$arguments               = (object) $arguments;
+$site                    = $arguments->site;
+$environment             = $arguments->environment;
+$updates_enabled         = $arguments->updates_enabled;
+$updates_exclude_themes  = $arguments->updates_exclude_themes;
+$updates_exclude_plugins = $arguments->updates_exclude_plugins;
 
 $lookup = ( new CaptainCore\Sites )->where( [ "site" => $site ] );
 
@@ -56,99 +62,3 @@ if ( $debug == "true" ) {
 }
 
 $response = shell_exec( $command );
-
-// Loads CLI configs
-$json = "{$_SERVER['HOME']}/.captaincore/config.json";
-
-if ( ! file_exists( $json ) ) {
-	echo "Error: Configuration file not found.";
-	return;
-}
-
-$config_data = json_decode ( file_get_contents( $json ) );
-$system      = $config_data[0]->system;
-$path        = $system->path;
-$new_logs    = [];
-
-foreach($config_data as $config) {
-	if ( isset( $config->captain_id ) and $config->captain_id == $captain_id ) {
-		$configuration = $config;
-		break;
-	}
-}
-
-if ( $system->captaincore_fleet == "true" ) {
-	$path = "{$path}/{$captain_id}";
-}
-
-// Define log file format
-$logs_path = "$path/{$site->site}_{$site->site_id}/{$environment}/updates";
-
-$responses = explode( "\n", $response );
-foreach ( $responses as $key => $item ) {
-    $time_now_file_name = date("Y-m-d-His");
-    $time_now           = date("Y-m-d H:i:s");
-    $data               = json_decode( $item );
-
-    // If JSON data not found, skip line
-    if ( ! is_array( $data ) ) {
-        continue;
-    }
-    if ( $key == "0" ) {
-        $type = "theme";
-    }
-    if ( $key == "1" ) {
-        $type = "plugin";
-    }
-
-    // Write to database
-    $update_log_add = [
-        "created_at"     => $time_now,
-        "site_id"        => $site->site_id,
-        "environment_id" => $environment_id,
-        "update_type"    => $type,
-        "update_log"     => json_encode( $data ),
-    ];
-    
-    // Update current environment with new data.
-    $log_id     = ( new CaptainCore\UpdateLogs )->insert( $update_log_add );
-
-    // Append to output
-    $new_logs[] = ( new CaptainCore\UpdateLogs )->get( $log_id );
-    
-    // Output to log file
-    if ( ! file_exists( "${logs_path}/" ) ) {
-        mkdir( "${logs_path}/", 0777, true );
-    }
-
-    file_put_contents( "${logs_path}/{$time_now_file_name}-{$type}s.json", json_encode( $data ), JSON_PRETTY_PRINT );
-}
-
-foreach( $new_logs as $new_log ) {
-
-    // Prepare request to API
-    $request = [
-        'method'  => 'POST',
-        'headers' => [ 'Content-Type' => 'application/json' ],
-        'body'    => json_encode( [ 
-            "command" => "update-log-add",
-            "site_id" => $site->site_id,
-            "token"   => $configuration->keys->token,
-            "data"    => $new_log,
-        ] ),
-    ];
-
-    if ( $system->captaincore_dev ) {
-        $request['sslverify'] = false;
-    }
-
-    // Post to CaptainCore API
-    $response = wp_remote_post( $configuration->vars->captaincore_api, $request );
-    if ( is_wp_error( $response ) ) {
-        $error_message = $response->get_error_message();
-        echo "Something went wrong: $error_message";
-        return;
-    }
-    echo $response['body'];
-
-}
