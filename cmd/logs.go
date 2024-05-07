2a7e898c5dc37f2b13550c39e73e004cb2cdbe53
Austin Ginder <austinginder@gmail.com>
2024-05-07 18:22:07 -0400

    📦 NEW: Commands `captaincore logs list` and `captaincore logs get`

---
+++ cmd/logs.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"errors"
+
+	"github.com/spf13/cobra"
+)
+
+var logsCmd = &cobra.Command{
+	Use:   "logs",
+	Short: "Logs commands",
+}
+
+var logsListCmd = &cobra.Command{
+	Use:   "list <site>",
+	Short: "Retrieve list of server logs for a site",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("requires a <site> argument")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		resolveCommand(cmd, args)
+	},
+}
+
+var logsGetCmd = &cobra.Command{
+	Use:   "get <site> --file=<filename>",
+	Short: "Retrieves server log file for a site",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("requires a <site> argument")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		resolveCommand(cmd, args)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(logsCmd)
+	logsCmd.AddCommand(logsListCmd)
+	logsCmd.AddCommand(logsGetCmd)
+	logsGetCmd.Flags().StringVar(&flagFile, "file", "", "File to retrieve")
+}
